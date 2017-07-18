1c9ef668927d380d9556dd01db16ff0bdd43e6a0
ksivaganesh99 <ksivaganesh99@gmail.com>
2017-07-18 20:44:51 +0530

    major change

--- README.md
+++ README.md
@@ -1,1 +1,78 @@
-"# BotChallenge" 
+
+
+
+
+This is a simple Slack bot to monitor your AWS resources. It is designed using AWS LEX and Lambda functions.
+
+
+Pain areas to solve using this bot:
+
+1. Usually AWS account users forget to stop the instances when not in use or before leaving the office which leads to surprise surge in the month end bill.
+2. As every team member doesn't have access to the AWS Billing and Cost Management service, they are unaware of the billing threshold limits which leads to over utilization of resources.
+3. Users forget to properly tag the instances while launching them which is not acceptable(Tags are used to segregate the overall AWS bill).
+
+Sloution developed:
+
+1. Everyday at 6:30 pm we have scheduled an event to trigger the AWS Lambda function which posts all the running instance at that particular time to the Slack team. We can stop the instances without logging into AWS console.
+2. Using Cloudwatch billing  matrics we have set a threshold for the estimated charges and have sent a notification to slack team via AWS SNS and lambda function.
+3. Using Cloudwatch ec2 events API we automated instance tagging. Simillarly auto-tagging for the other resources can be done on their creation.
+
+
+Enhancements:
+
+1. We need to add authorization to stop and launch the instances(As of know we are setting up a proper service role to Lambda function). We are planning to leverage Cognito Identity services for authentication.
+2. We should be able to get the billing details from Lex bot and take appropriate action.
+3. Planning to tag the resources through Slack bot.
+
+
+
+
+Create a Lex bot using the LEX definition file and also create a Lambda function using this repo.
+
+
+Give appropriate role to Lambda Function to access all the resources.
+
+
+We need a Slack app created and configured to integrate with LEX. Please follow the instructions given in the below link.
+
+http://docs.aws.amazon.com/lex/latest/dg/slack-bot-association.html
+
+Create an incoming webhook url for your group and save it for later use.
+
+I assume you have the Slack and LEX integration ready and able to communicate. Now let us go through the solutions one by one.
+
+1. AWS unstopped resource notifier:
+
+Create an event rule in CloudWatch to trigger a Lambda function every day at some time. This Lambda function reads the running instances from all the regions and send them to Slack team using incoming webhook URL you saved before.
+Ping from the Lambda function contains buttons whose actions are posted to interative webhoot URL. One point to mention we should have an API gateway configured to the same Lambda function whose endpoint url is used to tackle interactive messages.
+While configuring API gateway important point is, In integration request give body template as :
+																Content Type : application/x-www-form-urlencoded
+																{
+																	"postBody" : $input.json("$")
+																}
+
+Give this endpoint URL in SLack Interactive Messages request URL.																
+
+At any point of time if we want to know the running instances we can ping the app which inturn is a bot. This returns the instances running  at that point of time.
+ 
+
+2. Billing alert:
+
+Create an alarm in CloudWatch to trigger the above Lambda Function when the billing metrics cross some threshold. This function posts a message to slack webhook URL.
+
+
+Uploading Bot Lambda Function:
+
+Run 'npm install' command  after opening the code. Zip it and upload it to Lambda functions with the default configuartions.
+
+
+
+
+
+
+
+
+
+
+
+<a href="https://slack.com/oauth/authorize?&client_id=71311666592.171951787604&scope=bot,chat:write:bot,channels:history,groups:history,im:history,mpim:history,incoming-webhook,commands,chat:write:user"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>
--- getinstances.js
+++ getinstances.js
@@ -1,7 +1,6 @@
 var qs = require('qs');
 var events = require('events');
 var request = require('request');
-//var Promise = require('Promise');
 var AWS = require('aws-sdk');  
 
 AWS.config.region = 'ap-southeast-1';  
@@ -30,7 +29,7 @@ var responsemessgae={};
     var eventEmitter = new events.EventEmitter();
     
     console.log("\n\nLoading handler\n\n");
-    //console.log(event.postBody);
+
 
     var ec2 = new AWS.EC2();
     
@@ -41,7 +40,6 @@ var getInstancesByregion = function(regionname,count){
     AWS.config.region = regionname ;
     console.log(AWS.config.region);
         var ec2 = new AWS.EC2().describeInstances();
-        //global.promise = global.promise+count;
          global["prom"+count]  = ec2.promise();
          
      
@@ -70,9 +68,7 @@ eventEmitter.on("updateregion",getInstancesByregion);
                  if (err) console.log(err, err.stack); // an error occurred
         else{
             var reservations = data.Reservations;
-            if(reservations.length > 0){
-                //console.log(reservations[0].Instances);
-            }
+           
             
             instances = [];
             reservations.forEach(function(reservation){
@@ -82,15 +78,14 @@ eventEmitter.on("updateregion",getInstancesByregion);
            console.log(instances.length);
             instances.forEach(function(instance){
                  instance.forEach(function(instan){
-               // console.log(instan)
-               // console.log(instan.Tags);
+              
                 instan.Tags.forEach(function(tag){
-                   // console.log(tag.Key+" "+tag.Value);
+                 
                  ownerTag = tag.Key === "Owner" ? tag.Value : '--';
                  nameTag = tag.Key === "Name" ? tag.Value : '--';
                 })
-                var valuenew = instan.InstanceId;
                 var availability = instan.Placement.AvailabilityZone;
+                  var valuenew = {"instanceid" : instan.InstanceId,"region" :availability };
                 if(instan.State.Name === "running"){
 
                 payloadopt.push( {
@@ -99,67 +94,34 @@ eventEmitter.on("updateregion",getInstancesByregion);
                 "buttons":[ 
                  {
                     "text":"Stop",
-                    "value":valuenew
+                    "value":JSON.stringify(valuenew)
                  }
               ]
         });
         }	
-          // console.log(instan.State.Name);
+    
            });
             });
            
-         // console.log(instances);
-            //console.log("\n\n" + instances + "\n\n"); // successful response
+       
                 }
   
-       // context.done(null, 'Function Finished!');  
-               //  console.log("Value 0 is " + values[0].toString);
-               // console.log("Value 1 is " + values[1].toString);
-                //console.log("Value 2 is " + values[2].toString);
+      
                 });
 
               
    
             console.log("asdkaldjoj");
          responsemessgae = {"text":"There are total "+payloadopt.length+" running instancecs.\n Please stop them if not in use","attachments":payloadopt};
-       // console.log(responsemessgae);
+       
         resolve(responsemessgae);
-       // return responsemessgae;
+       
             });
 
  
-           console.log("success");
-          // return "Hello World";
+        
         });
 
-   /* ec2.describeInstances(function(err, data) {
-        console.log("\nIn describe instances:\n");
-        if (err) console.log(err, err.stack); // an error occurred
-        else{
-            var reservations = data.Reservations;
-            console.log(reservations.length);
-            for(var i=0;i<reservations.length;i++){
-                //console.log(reservations[i]);
-             instances.push(reservations[i].Instances);
-            }
-             for(var j=0;j<instances.length;j++){
-           console.log(instances[j][0].State.Name);
-             }
-            //console.log("\n\n" + instances + "\n\n"); // successful response
-                }
-        
-       // context.done(null, 'Function Finished!');  
-    });*/
-     
-   // var params = {InstanceIds: [instanceid]};
-   // console.log(params);
-
- /* message = {
-        "text": name+" has requested to stop the instance "+instanceid+" and the current status is stopping",
-        "replace_original": false
-    }*/
-   //console.log(message);
-//context.done(null, message); 
-//return "hello";
+   
 });
 };  
